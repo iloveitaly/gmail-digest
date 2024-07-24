13bb180f25f85e4918f179388b92194b61939c16
Michael Bianco <iloveitaly@gmail.com>
2024-07-24 12:57:39 -0600

    basic apscheduler config

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+
+import click
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from github_overlord import cli
+
+
+def handle_click_exit(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except click.exceptions.Exit as e:
+            if e.exit_code != 0:
+                raise
+
+    return wrapper
+
+
+def job():
+    for command in list(cli.commands.values()):
+        handle_click_exit(command)()
+
+
+def cron():
+    schedule = os.environ.get("SCHEDULE", "0 6 * * *")
+    print(f"Running on schedule: {schedule}")
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(job, CronTrigger.from_crontab(schedule))
+    scheduler.start()
+
+
+if __name__ == "__main__":
+    cron()
